2d075ae5418840b8532af5845c93da44ab0f8ea3
Krzysztof Garbowicz <kgar@netcompany.com>
2023-06-25 11:54:13 +0200

    only deploy grafana; use istioctl

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -8,63 +8,63 @@ module "namespaces" {
   labels         = each.value.labels
 }
 
-module "istio_base" {
-  depends_on = [module.namespaces]
+# module "istio_base" {
+#   depends_on = [module.namespaces]
 
-  source     = "../Shared-Modules/helm-chart"
-  name       = "istio-base"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "base"
+#   source     = "../Shared-Modules/helm-chart"
+#   name       = "istio-base"
+#   repository = "https://istio-release.storage.googleapis.com/charts"
+#   chart      = "base"
 
-  chart_version = var.chart_config_istio_base.version
-  namespace     = var.chart_config_istio_base.namespace
+#   chart_version = var.chart_config_istio_base.version
+#   namespace     = var.chart_config_istio_base.namespace
 
-  set_maps = var.chart_config_istio_base.set_maps
-}
+#   set_maps = var.chart_config_istio_base.set_maps
+# }
 
-module "istiod" {
-  depends_on = [module.istio_base]
+# module "istiod" {
+#   depends_on = [module.istio_base]
 
-  source     = "../Shared-Modules/helm-chart"
-  name       = "istiod"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "istiod"
+#   source     = "../Shared-Modules/helm-chart"
+#   name       = "istiod"
+#   repository = "https://istio-release.storage.googleapis.com/charts"
+#   chart      = "istiod"
 
-  chart_version = var.chart_config_istiod.version
-  namespace     = var.chart_config_istiod.namespace
+#   chart_version = var.chart_config_istiod.version
+#   namespace     = var.chart_config_istiod.namespace
 
-  set_maps = var.chart_config_istiod.set_maps
-}
+#   set_maps = var.chart_config_istiod.set_maps
+# }
 
-module "istio_ingressgateway" {
-  depends_on = [module.istiod]
+# module "istio_ingressgateway" {
+#   depends_on = [module.istiod]
 
-  source     = "../Shared-Modules/helm-chart"
-  name       = "istio-ingressgateway"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "gateway"
+#   source     = "../Shared-Modules/helm-chart"
+#   name       = "istio-ingressgateway"
+#   repository = "https://istio-release.storage.googleapis.com/charts"
+#   chart      = "gateway"
 
-  chart_version = var.chart_config_istio_ingressgateway.version
-  namespace     = var.chart_config_istio_ingressgateway.namespace
+#   chart_version = var.chart_config_istio_ingressgateway.version
+#   namespace     = var.chart_config_istio_ingressgateway.namespace
 
-  set_maps = var.chart_config_istio_ingressgateway.set_maps
-}
+#   set_maps = var.chart_config_istio_ingressgateway.set_maps
+# }
 
-module "kube_prometheus_stack" {
-  depends_on = [module.istio_ingressgateway]
+# module "kube_prometheus_stack" {
+#   depends_on = [module.istio_ingressgateway]
 
-  source        = "../Shared-Modules/helm-chart"
-  name          = "kube-prometheus"
-  repository    = "https://charts.bitnami.com/bitnami"
-  chart         = "kube-prometheus"
-  chart_version = var.chart_config_kube_prometheus_stack.version
+#   source        = "../Shared-Modules/helm-chart"
+#   name          = "kube-prometheus"
+#   repository    = "https://charts.bitnami.com/bitnami"
+#   chart         = "kube-prometheus"
+#   chart_version = var.chart_config_kube_prometheus_stack.version
 
-  namespace = var.chart_config_kube_prometheus_stack.namespace
+#   namespace = var.chart_config_kube_prometheus_stack.namespace
 
-  timeout = var.chart_config_kube_prometheus_stack.timeout
+#   timeout = var.chart_config_kube_prometheus_stack.timeout
 
-  set_maps = var.chart_config_kube_prometheus_stack.set_maps
-}
+#   set_maps = var.chart_config_kube_prometheus_stack.set_maps
+# }
 
 module "grafana_public_ip" {
   depends_on = [module.kube_prometheus_stack]
@@ -128,19 +128,19 @@ module "grafana" {
 #   set_maps = var.chart_config_kiali_operator.set_maps
 # }
 
-module "kiali_server" {
-  depends_on = [module.grafana]
+# module "kiali_server" {
+#   depends_on = [module.grafana]
 
-  source        = "../Shared-Modules/helm-chart"
-  name          = "kiali-server"
-  repository    = "https://kiali.org/helm-charts"
-  chart         = "kiali-server"
-  chart_version = var.chart_config_kiali_server.version
+#   source        = "../Shared-Modules/helm-chart"
+#   name          = "kiali-server"
+#   repository    = "https://kiali.org/helm-charts"
+#   chart         = "kiali-server"
+#   chart_version = var.chart_config_kiali_server.version
 
-  namespace = var.chart_config_kiali_server.namespace
+#   namespace = var.chart_config_kiali_server.namespace
 
-  values = concat(var.chart_config_kiali_server.values)
+#   values = concat(var.chart_config_kiali_server.values)
 
-  set_maps = var.chart_config_kiali_server.set_maps
-}
+#   set_maps = var.chart_config_kiali_server.set_maps
+# }
 
